95d493dcf6f1459486583e151388aadfe3dc681e
Jon Etchison <jetch5@yahoo.com>
2019-09-17 21:57:16 -0500

    completed with errors

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,75 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+//import org.launchcode.models.CategoryType;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "My Categories");
+
+        return "category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute(new Category());
+//        model.addAttribute("categoryTypes", CategoryType.values());
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(@ModelAttribute  @Valid Category Category,
+                                       Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+
+        categoryDao.save(Category);
+        return "redirect:/category";
+    }
+
+//    @RequestMapping(value = "remove", method = RequestMethod.GET)
+//    public String displayRemoveCheeseForm(Model model) {
+//        model.addAttribute("cheeses", cheeseDao.findAll());
+//        model.addAttribute("title", "Remove Cheese");
+//        return "cheese/remove";
+//    }
+//
+//    @RequestMapping(value = "remove", method = RequestMethod.POST)
+//    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+//
+//        for (int cheeseId : cheeseIds) {
+//            cheeseDao.delete(cheeseId);
+//        }
+//
+//        return "redirect:";
+//    }
+
+}
